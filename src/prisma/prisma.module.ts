b8b4bc1b2dbd2d291079549717c3dbc0f3a75cc4
Farsit-007 <robayatfarsit@gmail.com>
2025-09-28 13:59:12 +0600

    Updated

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService], // Register PrismaService as a provider inside this module
+  exports: [PrismaService], // Export the Services so other modules can use it
 })
 export class PrismaModule {}
