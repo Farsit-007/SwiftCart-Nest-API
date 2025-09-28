14d6c809fcb037c4ad3f5e5751eeff4c0a98feaf
Farsit-007 <robayatfarsit@gmail.com>
2025-09-28 13:51:59 +0600

    Updated

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -10,14 +10,18 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { ClientInfoMiddleware } from 'src/common/middleware/client-info.middleware';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [PrismaModule], // Import Prisma so UserService can use database
+  controllers: [UserController], // Register UserController to handle routes
+  providers: [UserService], // Register UserService as a provider
 })
 export class UserModule implements NestModule {
+  // UserModule implements NestModule so we can configure middleware inside it.
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(ClientInfoMiddleware)
-      .forRoutes({ path: 'users/register', method: RequestMethod.POST });
+      .apply(ClientInfoMiddleware) // Apply the ClientInfoMiddleware
+      .forRoutes({
+        path: 'users/register', // Target only the route POST /users/register
+        method: RequestMethod.POST, // Middleware applies ONLY for POST requests
+      });
   }
 }
