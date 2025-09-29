05a53ca011837547f66c68776f256dd06a819d05
Farsit-007 <robayatfarsit@gmail.com>
2025-09-29 22:06:01 +0600

    Updated

--- src/user/providers/find-one-user-by-email.provider.ts
+++ src/user/providers/find-one-user-by-email.provider.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FindOneUserByEmailProvider {}
+export class FindOneUserByEmailProvider {
+  constructor(private readonly prisma: PrismaService) {}
+  public async findOneByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({ where: { email: email } });
+    if (!user) {
+      throw new UnauthorizedException('User dose not exists');
+    }
+    return user;
+  }
+}
