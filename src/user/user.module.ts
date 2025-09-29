c68dbbf1b36413a96b136cbef6764deaaf6efed3
Farsit-007 <robayatfarsit@gmail.com>
2025-09-29 21:55:02 +0600

    Updated

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -11,11 +11,12 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { ClientInfoMiddleware } from 'src/common/middleware/client-info.middleware';
 import { CreateUserProvider } from './providers/create-user.provider';
 import { AuthModule } from 'src/auth/auth.module';
+import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email.provider';
 
 @Module({
   imports: [PrismaModule, forwardRef(() => AuthModule)], // Import Prisma so UserService can use database
   controllers: [UserController], // Register UserController to handle routes
-  providers: [UserService, CreateUserProvider], // Register UserService as a provider
+  providers: [UserService, CreateUserProvider, FindOneUserByEmailProvider], // Register UserService as a provider
   exports: [UserService],
 })
 export class UserModule implements NestModule {
