713aa2a6857fba5d7655752608e111410606a5aa
Farsit-007 <robayatfarsit@gmail.com>
2025-10-02 02:34:05 +0600

    Updated

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,3 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
+import { AUTH_TYPE_KEY } from '../constant/auth.constant';
+import { AuthType } from '../enums/auth-type.enum';
 
-export const Auth = (...args: string[]) => SetMetadata('auth', args);
+export const Auth = (...authType: AuthType[]) =>
+  SetMetadata(AUTH_TYPE_KEY, authType);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
+  @Auth(AuthType.None)
   public createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createrUser(createUserDto);
   }
