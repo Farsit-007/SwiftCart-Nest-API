451e2b25243e6d5157e38e074243502e0c34cf4c
Farsit-007 <robayatfarsit@gmail.com>
2025-10-02 02:37:29 +0600

    Updated

--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -2,9 +2,12 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { AccessTokenGuard } from '../access-token/access-token.guard';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+  // Set the default Auth Type
+  private static readonly defaultAuthType = AuthType.Bearer
   constructor(
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
