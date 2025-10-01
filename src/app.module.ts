7121088071be8fe950ea2b09f73395ddc87dde7a
Farsit-007 <robayatfarsit@gmail.com>
2025-10-02 02:02:54 +0600

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
+import jwtConfig from './auth/config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards/access-token/access-token.guard';
 
 @Module({
   imports: [
@@ -18,8 +22,16 @@ import appConfig from './config/app.config';
       load: [appConfig],
       // validationSchema : validationFile // If need
     }),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
