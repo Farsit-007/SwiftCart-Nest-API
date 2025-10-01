60e82c085ec8c628a09c919f0a8bd87a6b908cc8
Farsit-007 <robayatfarsit@gmail.com>
2025-10-02 02:45:17 +0600

    Updated

--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -1,18 +1,27 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+/* eslint-disable no-unused-labels */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AccessTokenGuard } from '../access-token/access-token.guard';
 import { AuthType } from 'src/auth/enums/auth-type.enum';
+import { Reflector } from '@nestjs/core';
+import { AUTH_TYPE_KEY } from 'src/auth/constant/auth.constant';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
   // Set the default Auth Type
   private static readonly defaultAuthType = AuthType.Bearer;
+
   // Create authTypeGuardMap
   private readonly authTypeGuardMap: Record<
     AuthType,
     CanActivate | CanActivate[]
   >;
+
   constructor(
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
@@ -24,6 +33,29 @@ export class AuthenticationGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    return true;
+    // authTypes from reflector
+    const authTypes = this.reflector.getAllAndOverride<AuthType[]>(
+      AUTH_TYPE_KEY,
+      [context.getHandler(), context.getClass()],
+    ) ?? [AuthenticationGuard.defaultAuthType];
+
+    // array of guards
+    const guards = authTypes.map((type) => this.authTypeGuardMap[type]).flat();
+
+    // Default Error
+    const error = new UnauthorizedException();
+
+    // Loop guards canActivate
+    for (const instance of guards) {
+      const canActivate = await Promise.resolve(
+        instance.canActivate(context),
+      ).catch((err: any) => {
+        error: err;
+      });
+      if (canActivate) {
+        return true;
+      }
+    }
+    throw error;
   }
 }
