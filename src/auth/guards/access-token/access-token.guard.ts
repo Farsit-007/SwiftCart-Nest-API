0d6aed945e1a6045f8814728b470323a7257ceec
Farsit-007 <robayatfarsit@gmail.com>
2025-10-01 22:01:14 +0600

    Updated

--- src/auth/guards/access-token/access-token.guard.ts
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -1,11 +1,34 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // Extract the request from the execution context
+    const request = context.switchToHttp().getRequest();
+    // Extract the token from header
+    // Validate the token
     return true;
+  }
+  private extractRequestFromHeader(request: Request): string | undefined {
+    const [_, token] = request.headers.authorization?.split(' ') ?? [];
+    return token;
   }
 }
