247537806956ebea767c83a69e9fb744dda37af6
Farsit-007 <robayatfarsit@gmail.com>
2025-10-02 01:56:54 +0600

    Updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { AccessTokenGuard } from 'src/auth/guards/access-token/access-token.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(AccessTokenGuard)
   @Post('register')
   public createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createrUser(createUserDto);
