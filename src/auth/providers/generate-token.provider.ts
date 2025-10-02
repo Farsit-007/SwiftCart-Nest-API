6f845b5f167d7d87d15a2ef46570ef1128c9c0a0
Farsit-007 <robayatfarsit@gmail.com>
2025-10-02 11:11:12 +0600

    Updated

--- src/auth/providers/generate-token.provider.ts
+++ src/auth/providers/generate-token.provider.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
 
 @Injectable()
-export class GenerateTokenProvider {}
+export class GenerateTokenProvider {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+  public async signToken<T>(userId: string, expiresIn: number, payload?: T) {
+    return await this.jwtService.signAsync(
+      {
+        id: userId,
+        ...payload,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: expiresIn,
+      },
+    );
+  }
+}
