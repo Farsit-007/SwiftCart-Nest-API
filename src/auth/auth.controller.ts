eb2b13878e69cf090f4323af5dedf226a6414445
Farsit-007 <robayatfarsit@gmail.com>
2025-10-03 12:54:47 +0600

    Updated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignDto } from './dtos/sign.dto';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,7 +15,7 @@ export class AuthController {
 
   @Post('refresh-token')
   @HttpCode(HttpStatus.OK)
-  public async refreshToken(@Body() signInDto: SignDto) {
-    return this.authService.signIn(signInDto);
+  public async refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshToken(refreshTokenDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { SignDto } from './dtos/sign.dto';
 import { SignInProvider } from './providers/sign-in.provider';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
+import { RefreshTokensProvider } from './providers/refresh-tokens.provider';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly signInProvider: SignInProvider) {}
+  constructor(
+    private readonly signInProvider: SignInProvider,
+    private readonly refreshTokenProvider: RefreshTokensProvider,
+  ) {}
 
   public async signIn(signInDto: SignDto) {
     return await this.signInProvider.signIn(signInDto);
+  }
+
+  public async refreshToken(refreshTokenDto: RefreshTokenDto) {
+    return await this.refreshTokenProvider.refreshtokens(refreshTokenDto);
   }
 }
